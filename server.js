3281bc082d6d8a38c30f8817b1d0573456bbbfb6
Kyle Lee <kyleleeca@gmail.com>
2022-07-19 01:53:20 -0400

    Add server module

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+
+const app = express()
+const port = process.env.port || 5000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.get('/api/hello', (req, res) => {
+    res.send({message: 'Server module Added!'})
+})
+
+app.listen(port, () => console.log(`listening on port ${port}`))
